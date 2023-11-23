af0a50d3a052e611784523a4e041a4be97b3baaf
MemphisAton <MemphisAton@gmail.com>
2023-11-23 22:33:31 +0300

    first commit

--- MySpaceXProject/settings.py
+++ MySpaceXProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 import content_manager.apps
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z+gztkm$7@#zdua0@mw*z*&11t+8z&*^anpb3bl&-@@l56lcl5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://ec2-51-20-136-212.eu-north-1.compute.amazonaws.com']
 
 # Application definition
 
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
